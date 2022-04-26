f894b08e5d35b83d570a51a9d7f9181a4a877e30
JoDxi <shahafxd@gmail.com>
2022-04-26 16:01:07 +0300

    Excercise_GOLD

---
+++ Python/Week 1/ExcerciseGOLD.py
@@ -0,0 +1,18 @@
+# Excercise 1
+# hw = 'Hello World! '
+# py = 'I love Python! '
+#
+# print (hw +py * 4)
+
+
+# # Excercise 2 - What is the Season
+# month = int(input('What month is it? (1-12)'))
+#
+# if month >= 3 and month <= 5:
+#     print('Its spring time!')
+# elif month >= 6 and month <=8:
+#     print('Its summer time!')
+# elif month >= 9 and month <=11:
+#     print('Its Autumn time!')
+# else:
+#     print("Winter is coming")
