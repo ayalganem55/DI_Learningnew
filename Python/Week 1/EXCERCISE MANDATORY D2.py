4f99263912dab5013f6f3f1cc1999cd6e6d9c7ae
JoDxi <shahafxd@gmail.com>
2022-04-26 15:37:34 +0300

    Hackathon#1

---
+++ Python/Week 1/EXCERCISE MANDATORY D2.py
@@ -0,0 +1,61 @@
+# Excercise 1 - Hello world
+# hw = 'Hello World! '
+# print(hw * 4)
+
+
+# Excercise 2 - Some Math 
+# pow (99, 3) * 8 
+
+
+# Excercise 3 - What is the output
+# 5 < 3   ---  False cus 5 is not smaller than 3 
+#  3 == 3  --- true 
+# 3 == "3"  --- false, cant compare intergar to str 
+# "3" > 3   --- false, integar to string 
+# "Hello" == "hello"   --- false , case sensetive 
+
+
+# Excercise 4 - computer brand 
+# computer_brand = 'Lenovo' 
+# print ('I have a' + computer_brand + ' computer')
+
+
+# Excercise 5 - Your Information 
+# name = 'Shachaf'
+# age = 25
+# shoe_size = 45
+# info = 'I enjoy Scuba Diving'
+# print ("My name is %s" % (name) + ", I am %s" % (age) + " years old. My shoe size is %s" % (shoe_size) + ' and in my spare time ' + info)
+
+
+# Excercise 6 - A  & B 
+# A = 15
+# B = 10 
+# if A > B:
+#     print('Hello World')
+
+
+
+# Excercise 7 - Odd or Even
+# number = int(input('Please choose a number'))
+# if number % 2 == 0 :
+#     print ('Even!')
+# else:
+#     print ("Odd!")
+
+
+# Exercise 8 : What’s Your Name ?
+# my_name = 'shachaf'
+# user_name = str(input('What is your name?').lower())
+# if my_name == user_name:
+#     print('Brother!')
+#
+
+# Excerise 9 - Tall enough to Ride
+# user_height = int(input('What is your height in Inches? '))
+# cent_value = 2.54 * user_height
+# min_height = 145
+# if cent_value > min_height:
+#     print('Enjoy your ride! ')
+# else:
+#     print('Sorry, come back next year ')
---
+++ Python/Week 1/Mandatory_dailyD2.py
@@ -0,0 +1,21 @@
+# string_user = str(input('Tell us something, at least 10 characters long'))
+# string_length = len(string_user)
+#
+# if string_length < 10:
+#     print(('Dont be shy, tell us some more'))
+#
+# else:
+#     print('You talk much')
+#
+# last_char = string_user[-1]
+# first_char = string_user[0]
+# print(first_char, last_char)
+#
+# for i in string_user:
+#     print(i)
+#
+#     import random
+#     l = list(string_user)
+#     random.shuffle(l)
+#     print(l)
+
