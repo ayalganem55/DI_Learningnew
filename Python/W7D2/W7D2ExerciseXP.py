5898eecf372ddc7e4cc784a19ae85ae24f0b06e1
JoDxi <shahafxd@gmail.com>
2022-05-12 13:38:41 +0300

    Mandatory Exercise W7D2

---
+++ Python/W7D2/W7D2ExerciseXP.py
@@ -0,0 +1,68 @@
+# Exercise 1 : What Are You Learning ?
+
+# def display_message(institue, subject):
+#     print('Im a student at ' + institue + ', we focus primarily learning ' + subject)
+#
+#
+# display_message('Developers Institute', 'Python')
+#
+#
+# # Exercise 2: What’s Your Favorite Book ?
+
+# def favorite_book(title):
+#     print('One of my favorite books is ' + title.title())
+#
+#
+# favorite_book('"the hunt for red october"')
+
+#  Exercise 3 : Some Geography
+
+# def describe_city(city, country='Australia'):
+#     print(city + ' is the capital of ' + country)
+#
+# describe_city('Tel Aviv', 'Israel')
+
+# Exercise 4 : Random
+import random
+
+#
+# def compare_number():
+#     number = (int(input("Choose a number between 0-100: ")))
+#     random_number = random.randint(0, 100)
+#     print(number, ' Is the chosen number.', random_number, 'Is the random number.', )
+#     if random_number == number:
+#         print('Both numbers are the same!')
+#     else:
+#         print('Whoops, no match!')
+#
+#
+# compare_number()
+
+
+# Exercise 5 : Let’s Create Some Personalized Shirts !
+#
+# def make_shirt(text_message='I love Python', size='Large'):
+#     print('Thank you for ordering a customized shirt, you ordered a size ' + size + ' with the text - ' + '"' + text_message + '"')
+#
+#
+# make_shirt(size='Medium')
+# make_shirt(size='Large')
+# make_shirt(size='Small', text_message='BoogaHoogas')
+
+
+# Exercise 6 : Magicians
+
+# magician_names = ['Robert', 'Chris', 'Debbie', 'Nicole']
+#
+# def show_magicians(magician_names):
+#     for magician in magician_names:
+#         print(magician)
+#
+# def make_great():
+#     for magician in magician_names:
+#         print("The Great - " + magician)
+#
+#
+# show_magicians(magician_names)
+# make_great()
+#
